1843928f0e7978559c1888e65b5827b10d2edf00
Nikita <nikreshetnjak@gmail.com>
2020-05-18 20:08:22 +0200

    Updates

---
+++ precache-manifest.2b0e21d27a385853ead576c98aee4121.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848548cd76892e32b12a5c59294d037f",
+    "url": "/nikita-reshetniak/telegram-app/index.html"
+  },
+  {
+    "revision": "4bd8121f8faea5329773",
+    "url": "/nikita-reshetniak/telegram-app/static/css/main.18de141d.chunk.css"
+  },
+  {
+    "revision": "5d094cf82fc107b01ee2",
+    "url": "/nikita-reshetniak/telegram-app/static/js/2.e35747bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nikita-reshetniak/telegram-app/static/js/2.e35747bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bd8121f8faea5329773",
+    "url": "/nikita-reshetniak/telegram-app/static/js/main.5aabf579.chunk.js"
+  },
+  {
+    "revision": "7d643048a9da0ca6071c",
+    "url": "/nikita-reshetniak/telegram-app/static/js/runtime-main.d6fb773a.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/nikita-reshetniak/telegram-app/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "34f88873f2da2bad6bd91a0204a7acb0",
+    "url": "/nikita-reshetniak/telegram-app/static/media/joaquin_phoenix.34f88873.jpg"
+  },
+  {
+    "revision": "8281db2d11316d214566848559450d26",
+    "url": "/nikita-reshetniak/telegram-app/static/media/keanu_reeves.8281db2d.jpg"
+  }
+]);
